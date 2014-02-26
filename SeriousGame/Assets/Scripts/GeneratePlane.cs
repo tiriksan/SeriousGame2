073797f05ddd69541e4d579fd6ee3a58db23cb20
tiriksan <kristian.aabrekk@gmail.com>
2014-02-26 14:03:48 +0100

    Random errors + planes

--- SeriousGame/Assets/Scripts/GeneratePlane.cs
+++ SeriousGame/Assets/Scripts/GeneratePlane.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using System;
 
-public class GeneratePlane : MonoBehaviour {
+public class GeneratePlane : MonoBehaviour
+{
 
     public GameObject[] planes;
     public float chanceToGetError = 5; //in %
@@ -20,5 +21,68 @@ public class GeneratePlane : MonoBehaviour {
     void generateErrors (PlaneCheckScriptTest plane)
     {
         //TODO
+        System.Random rng = new System.Random();
+        int rnd = rng.Next(0, 100);
+        if(rnd < chanceToGetError)
+        {
+            //set random error
+            if(plane.tag == "Cessna")
+            {
+                int error = rng.Next(0, 2);
+                switch(error)
+                {
+                    case 0:
+                        plane.centerEngineOilLeak = true;
+                        break;
+                    case 1:
+                        plane.centerEngineBroken = true;
+                        break;
+
+                }
+            } else if(plane.tag == "C130")
+            {
+                int error = rng.Next(0, 4);
+                switch(error)
+                {
+                    case 0:
+                    plane.leftEngineOilLeak = true;
+                    break;
+                    case 1:
+                    plane.leftEngineBroken = true;
+                    break;
+                    case 2:
+                    plane.rightEngineOilLeak = true;
+                    break;
+                    case 3:
+                    plane.rightEngineBroken = true;
+                    break;
+                }
+            } else if(plane.tag == "Learjet60")
+            {
+                int error = rng.Next(0, 6);
+                switch(error)
+                {
+                    case 0:
+                    plane.leftEngineBroken = true;
+                    break;
+                    case 1:
+                    plane.rightEngineBroken = true;
+                    break;
+                    case 2:
+                    plane.leftEngineOilLeak = true;
+                    break;
+                    case 3:
+                    plane.rightEngineOilLeak = true;
+                    break;
+                    case 4:
+                    plane.rightFlapBroken = true;
+                    break;
+                    case 5:
+                    plane.leftFlapBroken = true;
+                    break;
+
+                }
+            }
+        }
     }
 }
