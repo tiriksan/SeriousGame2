059abccadc1e2981c66160d4709e2e712dc23fa6
Jim-Espen Gundersen <jimgundersen@gmail.com>
2014-02-26 13:31:12 +0100

    Added a simple start menu

--- SeriousGame/Assets/Scripts/InitScript.cs
+++ SeriousGame/Assets/Scripts/InitScript.cs
@@ -7,13 +7,31 @@ public class InitScript : MonoBehaviour {
 	public GameObject cameraRotator;
 	public GameObject light;
     public GeneratePlane genPlane;
+	private bool menuVisible = true;
+
+	void menuStart()
+	{
+		genPlane.generatePlane();
+	}
+
+	void OnGUI()
+	{
+		if (menuVisible == true)
+		{
+			GUI.Box(new Rect(0,0,Screen.width,Screen.height),"Aircraft startup\nThe point of the game is to check the aircrafts for any errors before dispatch.");
+			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 30), "Start Game"))
+			{
+				menuStart();
+				menuVisible = false;
+			}
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
 		GameObject.Instantiate(airbase);
 		GameObject.Instantiate(cameraRotator);
 		GameObject.Instantiate(light);
-        genPlane.generatePlane();
 	}
 	
 	// Update is called once per frame
