c52afc49af02c3d8d37c034778487accc293e74b
tiriksan <kristian.aabrekk@gmail.com>
2014-02-26 11:07:27 +0100

    La til fly og starta på GeneratePlane

---
+++ SeriousGame/Assets/Scripts/GeneratePlane.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GeneratePlane : MonoBehaviour {
+
+    public GameObject[] planes;
+
+    public void generatePlane ()
+    {
+        //TODO
+        GameObject.Instantiate(planes[0]);
+    }
+}
--- SeriousGame/Assets/Scripts/InitScript.cs
+++ SeriousGame/Assets/Scripts/InitScript.cs
@@ -6,12 +6,14 @@ public class InitScript : MonoBehaviour {
 	public GameObject airbase;
 	public GameObject cameraRotator;
 	public GameObject light;
+    public GeneratePlane genPlane;
 
 	// Use this for initialization
 	void Start () {
 		GameObject.Instantiate(airbase);
 		GameObject.Instantiate(cameraRotator);
 		GameObject.Instantiate(light);
+        genPlane.generatePlane();
 	}
 	
 	// Update is called once per frame
