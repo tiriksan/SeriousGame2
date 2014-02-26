c3536dbd57c63be5b33bc15de322957dff664bab
tiriksan <kristian.aabrekk@gmail.com>
2014-02-26 11:43:20 +0100

    Generate random plane + cessna position

--- SeriousGame/Assets/Scripts/GeneratePlane.cs
+++ SeriousGame/Assets/Scripts/GeneratePlane.cs
@@ -1,13 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class GeneratePlane : MonoBehaviour {
 
     public GameObject[] planes;
+    public float chanceToGetError = 5; //in %
 
     public void generatePlane ()
     {
         //TODO
-        GameObject.Instantiate(planes[0]);
+        System.Random rng = new System.Random();
+        int plane = rng.Next(0, planes.Length);
+        Debug.Log(plane);
+        GameObject.Instantiate(planes[plane]);
     }
 }
