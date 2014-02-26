ad7cec8c63e45a59f08b041a37d78da42c121753
Jim-Espen Gundersen <jimgundersen@gmail.com>
2014-02-26 13:43:41 +0100

    Small update to startmenu

--- SeriousGame/Assets/Scripts/InitScript.cs
+++ SeriousGame/Assets/Scripts/InitScript.cs
@@ -18,8 +18,11 @@ public class InitScript : MonoBehaviour {
 	{
 		if (menuVisible == true)
 		{
-			GUI.Box(new Rect(0,0,Screen.width,Screen.height),"Aircraft startup\nThe point of the game is to check the aircrafts for any errors before dispatch.");
-			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 30), "Start Game"))
+			GUI.Box(new Rect(0,0,Screen.width,Screen.height),"\n\nAircraft startup\n\nThe point of the game is to " +
+				"check the aircrafts for any errors before dispatch. " +
+			        "\nUse the buttons on the bottom of the screen to issue commands to the pilot,\n " +
+				"and press and hold the right mouse button to look around the aircraft.");
+			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 40, 100, 30), "Start Game"))
 			{
 				menuStart();
 				menuVisible = false;
