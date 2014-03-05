3366fc52d68f94447f33b6b59d79123daab41e5d
Jim-Espen Gundersen <jimgundersen@gmail.com>
2014-03-05 10:03:45 +0100

    Fixed menu text layout, added escape key input for quitting game

--- SeriousGame/Assets/Scripts/InitScript.cs
+++ SeriousGame/Assets/Scripts/InitScript.cs
@@ -18,11 +18,11 @@ public class InitScript : MonoBehaviour {
 	{
 		if (menuVisible == true)
 		{
-			GUI.Box(new Rect(0,0,Screen.width,Screen.height),"\n\nAIRCRAFT STARTUP\n\nThe point of the game is to " +
+			GUI.Box(new Rect(0,0,Screen.width,Screen.height),"\n\n\n\n\n\nAIRCRAFT STARTUP\n\n\n\n\nThe point of the game is to " +
 				"check the aircrafts for any errors before dispatch. " +
-			        "\nUse the buttons on the bottom of the screen to issue commands to the pilot,\n " +
-				"and press and hold the right mouse button to look around the aircraft.");
-			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 40, 100, 30), "Start Game"))
+			        "\n\nUse the buttons on the bottom of the screen to issue commands to the pilot,\n\n " +
+			        "and press and hold the right mouse button to look around the aircraft.\n\n\n\nPress 'Esc' to quit.");
+			if (GUI.Button (new Rect (Screen.width / 2 - 50, Screen.height / 2 + 70, 100, 30), "Start Game"))
 			{
 				menuStart();
 				menuVisible = false;
@@ -39,6 +39,8 @@ public class InitScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(Input.GetKeyDown("escape")) {//When a key is pressed down it see if it was the escape key if it was it will execute the code
+			Application.Quit(); // Quits the game
+		}
 	}
 }
